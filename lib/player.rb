ae81e59e7325eb8208b8cd583bc046b37b351736
Tyler Blackmon <tyler.blackmon8@gmail.com>
2023-07-13 09:22:19 -0600

    feat: add monthly_cost and contract_length to player

--- lib/player.rb
+++ lib/player.rb
@@ -1,7 +1,9 @@
 class Player
-  attr_reader :first_name, :last_name
-  def initialize(name, monthly_cost, total_cost)
+  attr_reader :first_name, :last_name, :monthly_cost, :contract_length
+  def initialize(name, monthly_cost, contract_length)
     @first_name = name.split.first
     @last_name = name.split.last
+    @monthly_cost = monthly_cost
+    @contract_length = contract_length
   end
 end
